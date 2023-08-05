3ad7b3c509b7baf2cba3cb17618dd9d0e94b419a
Prem <premkumar.muthupandi@gmail.com>
2023-08-05 14:05:53 -0300

    Friest COmmit

---
+++ example1.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const originalArr = [1,2,3,4,5];
+
+const student =[
+    {
+    name:'prem',
+    age:22
+},
+
+{
+    naam:'arul',
+    age:20
+},
+
+
+]
+ 
+const newArr = student.filter(x=>(x.age));
+
+// newArr.filter(y=>y.)
+
+console.log(newArr);
